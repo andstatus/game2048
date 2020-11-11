d8128bdc4585e3c870619334c3b2970d6f98f5b9
Yuri Volkov <yvolk@yurivolkov.com>
2020-11-11 23:03:09 +0300

    Workaround of VfsFile issue https://github.com/korlibs/korge-next/issues/73

--- src/commonMain/kotlin/org/andstatus/game2048/StringResources.kt
+++ src/commonMain/kotlin/org/andstatus/game2048/StringResources.kt
@@ -3,6 +3,7 @@ package org.andstatus.game2048
 import com.soywiz.klogger.Console
 import com.soywiz.klogger.log
 import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korio.lang.FileNotFoundException
 import com.soywiz.korio.serialization.xml.Xml
 
 class StringResources private constructor(private val strings: Map<String, String>,
@@ -18,7 +19,15 @@ class StringResources private constructor(private val strings: Map<String, Strin
         private suspend fun loadLang(lang: String): Map<String, String> =
             "res/values${if (lang.isEmpty()) "" else "-"}$lang/strings.xml"
             .let { path -> resourcesVfs[path] }
-            .let { if (it.exists()) it.readString() else ""}
+            // In Android it.exists() always returns false, so we have to catch exception instead of below:
+            //    .let { if (it.exists()) it.readString() else ""}
+            .let {
+                try {
+                    it.readString()
+                } catch (e: FileNotFoundException) {
+                    ""
+                }
+            }
             .let { Xml(it) }
             .allChildren
             .mapNotNull { xml -> xml.attribute("name")?.let { it to xml.text } }
