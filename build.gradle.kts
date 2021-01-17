8ba895636a580095ef2c0a338609cf389ed16208
Yuri Volkov <yvolk@yurivolkov.com>
2021-01-17 09:21:46 +0300

    Update korgePlugin to v.2.0.0.2. Try to remove not very widely used repositories from Gradle scripts. Issue https://github.com/korlibs/korge-next/issues/121 created to allow removal of custom KorGe repository also.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,9 @@ buildscript {
 
     repositories {
         mavenLocal()
-        maven { url = uri("https://dl.bintray.com/korlibs/korlibs") }
         maven { url = uri("https://plugins.gradle.org/m2/") }
         mavenCentral()
         google()
-        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-dev") }
-        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
     }
     dependencies {
         classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
