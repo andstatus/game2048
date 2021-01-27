53762266363a5bfede7f6201bf7f827ef1b83bf2
Yuri Volkov <yvolk@yurivolkov.com>
2021-01-27 20:31:24 +0300

    Fix AI logic: add Computer's move

--- src/commonMain/kotlin/org/andstatus/game2048/ai/AiAlgorithm.kt
+++ src/commonMain/kotlin/org/andstatus/game2048/ai/AiAlgorithm.kt
@@ -4,7 +4,8 @@ enum class AiAlgorithm(val id: String) {
     RANDOM("random"),
     MAX_SCORE_OF_NEXT_MOVE("max_next_one"),
     MAX_SCORE_OF_N_MOVES("max_next_n"),
-    MAX_FREE_OF_N_MOVES("max_free_n");
+    MAX_FREE_OF_N_MOVES("max_free_n"),
+    LONGEST_RANDOM_PLAY("longest_random");
 
     companion object {
         fun load(value: String?): AiAlgorithm = AiAlgorithm.values()
--- src/commonMain/kotlin/org/andstatus/game2048/ai/AiPlayer.kt
+++ src/commonMain/kotlin/org/andstatus/game2048/ai/AiPlayer.kt
@@ -3,6 +3,7 @@ package org.andstatus.game2048.ai
 import org.andstatus.game2048.Settings
 import org.andstatus.game2048.model.Board
 import org.andstatus.game2048.model.GameModel
+import org.andstatus.game2048.model.MovesAndModel
 import org.andstatus.game2048.model.PlayerMoveEnum
 import org.andstatus.game2048.model.PlayerMoveEnum.Companion.UserMoves
 import kotlin.random.Random
@@ -14,9 +15,10 @@ class AiPlayer(val settings: Settings) {
         when(settings.aiAlgorithm) {
             AiAlgorithm.RANDOM -> allowedRandomMove(board)
             AiAlgorithm.MAX_SCORE_OF_NEXT_MOVE -> moveWithMaxScore(board)
-            AiAlgorithm.MAX_SCORE_OF_N_MOVES -> maxScoreNMoves(board, 10)
+            AiAlgorithm.MAX_SCORE_OF_N_MOVES -> maxScoreNMoves(board, 6)
             AiAlgorithm.MAX_FREE_OF_N_MOVES -> maxFreeNMoves(board, 8)
-    }
+            AiAlgorithm.LONGEST_RANDOM_PLAY -> longestRandomPlay(board)
+        }
 
     private fun fromBoard(board: Board): GameModel = GameModel(settings, board) { _, _ -> }
 
@@ -32,7 +34,7 @@ class AiPlayer(val settings: Settings) {
 
     private fun maxScoreNMoves(board: Board, nMoves: Int): PlayerMoveEnum {
         return playNMoves(board, nMoves)
-            .maxByOrNull(this::meanScoreForList)
+            .maxByOrNull(this::maxScoreForList)
             ?.key
             ?: allowedRandomMove(board)
     }
@@ -42,7 +44,7 @@ class AiPlayer(val settings: Settings) {
     }
 
     private fun meanScoreForList(entry: Map.Entry<PlayerMoveEnum, List<GameModel>>): Int {
-        return entry.value.sumBy(GameModel::score) / entry.value.size
+        return if (entry.value.isEmpty()) 0 else entry.value.sumBy(GameModel::score) / entry.value.size
     }
 
     private fun maxFreeNMoves(board: Board, nMoves: Int): PlayerMoveEnum {
@@ -69,10 +71,40 @@ class AiPlayer(val settings: Settings) {
         return models
     }
 
-    private fun playUserMoves(model: GameModel) = UserMoves
+    private fun longestRandomPlay(board: Board): PlayerMoveEnum {
+        val models: Map<PlayerMoveEnum, GameModel> = playUserMoves(fromBoard(board))
+            .fold(HashMap<PlayerMoveEnum, GameModel>()) { map, mm ->
+                mm.moves.firstOrNull()?.let {
+                    map.put(it.playerMoveEnum, mm.model)
+                    map
+                } ?: map
+            }
+            .mapValues { playRandomTillEnd(it.value) }
+
+        return models.maxByOrNull { it.value.usersMoveNumber }?.key ?: allowedRandomMove(board)
+    }
+
+    private fun playRandomTillEnd(modelIn: GameModel): GameModel {
+        var model = modelIn
+        while (!model.noMoreMoves()) {
+            model = allowedRandomMove(model.board)
+                .let(model::calcMove)
+                .let(model::play)
+                .let(this::playComputerMove)
+                .model
+        }
+        return model
+    }
+
+    private fun playUserMoves(model: GameModel): List<MovesAndModel> = UserMoves
         .map(model::calcMove)
         .filter { it.moves.isNotEmpty() }
         .map(model::play)
+        .map(this::playComputerMove)
+
+    private fun playComputerMove(mm: MovesAndModel): MovesAndModel {
+        return mm.model.randomComputerMove().let { MovesAndModel(mm.moves, it.model) }
+    }
 
     private fun allowedRandomMove(board: Board): PlayerMoveEnum {
         if (settings.allowUsersMoveWithoutBlockMoves) return randomMove()
